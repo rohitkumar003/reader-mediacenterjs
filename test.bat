eb2117d4c5bbf02940eb9c0df10f741661b4a126
Rohit Kumar <rohit.kumar.003@gmail.com>
2019-03-04 20:09:20 +0800

    test.bat

--- test.bat
+++ test.bat
@@ -1,22 +1,3 @@
-@echo off
-git rev-parse --short head > info.txt
-set /p commit=<info.txt
-git rev-parse --abbrev-ref head > info.txt
-set /p branch=<info.txt
-del info.txt
-set prefix=%branch%-%commit%
-rmdir /S /Q reader-mediacenterjs.wiki
-git clone https://github.com/rohitkumar003/reader-mediacenterjs.wiki.git
-cd reader-mediacenterjs.wiki
-echo checking if %prefix%.zip already exists.
-jar -cMf .\%prefix%.zip ..\build\debug
-find /c "%prefix%.zip" testpage.md && ( echo File %prefix%.zip exists. && exit ) 
-echo File %prefix%.zip does not exists.
-@echo: >> testpage.md 
-echo [%prefix%.zip](%prefix%.zip)>>testpage.md
-git status -s
-git add %prefix%.zip
-git add testpage.md
-git commit -m "Updating artifact"
-git push
-cd ..
+version https://git-lfs.github.com/spec/v1
+oid sha256:710a4f17b8d264d51be2d17cfac304c9a36c77d277aea89cdcfeeeee0eada1c0
+size 724
